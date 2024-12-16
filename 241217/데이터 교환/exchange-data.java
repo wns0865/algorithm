3a38383dc229482f69d0afb8840c4ca3d3284856
wns0865 <wns0865@naver.com>
2024-12-17 02:05:48 +0900

    Title: 데이터 교환, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241217/데이터 교환/exchange-data.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int a=5,b=6,c=7;
+       int oa=5,ob=6,oc=7;
+       b=oa;
+       c=ob;
+       a=oc;
+        System.out.print(a+"\n"+b+"\n"+c);
+    }
+}
