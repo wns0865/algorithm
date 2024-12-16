72dad951cef01c2b0efafacbaaebc5a15569988d
wns0865 <wns0865@naver.com>
2024-12-17 02:03:16 +0900

    Title: 정수 복사, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241217/정수 복사/copy-integer.java
+++ 241217/정수 복사/copy-integer.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a=3;
         int b=4;
-        a=b;
-        System.out.print(a*b);
+        b=a;
+        System.out.print(a+" "+b+"\n"+ a*b);
     }
 }
