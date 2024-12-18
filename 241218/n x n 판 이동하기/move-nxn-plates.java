c7dbef222218087e2e10805fda82b1b8b4a5361b
wns0865 <wns0865@naver.com>
2024-12-18 20:04:56 +0900

    Title: n x n 판 이동하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 241218/n x n 판 이동하기/move-nxn-plates.java
+++ 241218/n x n 판 이동하기/move-nxn-plates.java
@@ -2,61 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int dx[] = {0, 1}; // 오른쪽, 아래
-    static int dy[] = {1, 0};
-    static class Pos {
-        int x, y;
-        public Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static int N, ans;
-    static int[][] arr;
-    static boolean[][] isVisited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // N 값 먼저 초기화
-        arr = new int[N][N];
-        isVisited = new boolean[N][N]; // N 이후에 초기화
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        long[][] dp = new long[N][N]; // 경로의 수를 저장할 DP 배열
 
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            String num[] = s.split(" ");
+            String[] line = br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(num[j]);
+                arr[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        start(0, 0); // BFS 탐색 시작
-        System.out.println(ans);
-    }
+        // DP 초기화
+        dp[0][0] = 1;
 
-    static void start(int a, int b) {
-        Queue<Pos> q = new ArrayDeque<>();
-        q.add(new Pos(a, b));
+        // DP 계산
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 0) continue; // 더 이상 이동 불가한 지점
+                int step = arr[i][j];
 
-        while (!q.isEmpty()) {
-            Pos tmp = q.poll();
-            int num = arr[tmp.x][tmp.y];
+                // 오른쪽 이동
+                if (j + step < N) {
+                    dp[i][j + step] += dp[i][j];
+                }
 
-            for (int d = 0; d < 2; d++) {
-                int tx = tmp.x + dx[d] * num;
-                int ty = tmp.y + dy[d] * num;
-
-                if (!check(tx, ty)) continue; // 범위 체크
-                if (tx == N - 1 && ty == N - 1) { // 도착 지점
-                    ans++;
-                } else if (!isVisited[tx][ty]) { // 방문하지 않은 경우
-                    isVisited[tx][ty] = true;
-                    q.add(new Pos(tx, ty));
+                // 아래로 이동
+                if (i + step < N) {
+                    dp[i + step][j] += dp[i][j];
                 }
             }
         }
-    }
 
-    static boolean check(int a, int b) {
-        return a >= 0 && a < N && b >= 0 && b < N; // 범위 확인
+        // 결과 출력 (오른쪽 아래 최종 값)
+        System.out.println(dp[N - 1][N - 1]);
     }
 }
